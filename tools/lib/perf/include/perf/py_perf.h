2afae3577d368645ed71077364b2606e860a25b9
Gautam Menghani <gautam@linux.ibm.com>
2025-02-01 21:30:20 +0530

    Support for more libperf API functions

--- tools/lib/perf/include/perf/py_perf.h
+++ tools/lib/perf/include/perf/py_perf.h
@@ -28,6 +28,73 @@ static PyTypeObject py_perf_thread_map_type = {
 	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
 };
 
+// perf_evlist declarations
+
+typedef struct {
+	PyObject_HEAD
+	struct perf_evlist *ptr;
+} py_perf_evlist;
+
+static void py_perf_evlist_dealloc(py_perf_evlist *evlist)
+{
+	free(evlist->ptr);
+	Py_DECREF(evlist);
+	PyObject_Del((PyObject *)evlist);
+}
+
+static PyTypeObject py_perf_evlist_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "libperf.py_perf_evlist",
+	.tp_doc = "Perf evlist object",
+	.tp_basicsize = sizeof(py_perf_evlist),
+	.tp_dealloc = (destructor)py_perf_evlist_dealloc,
+	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+};
+
+// perf_evsel declarations
+typedef struct {
+	PyObject_HEAD
+	struct perf_evsel *ptr;
+} py_perf_evsel;
+
+static void py_perf_evsel_dealloc(py_perf_evsel *evsel)
+{
+	free(evsel->ptr);
+	Py_DECREF(evsel);
+	PyObject_Del((PyObject *)evsel);
+}
+
+static PyTypeObject py_perf_evsel_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "libperf.py_perf_evsel",
+	.tp_doc = "Perf evsel object",
+	.tp_basicsize = sizeof(py_perf_evsel),
+	.tp_dealloc = (destructor)py_perf_evsel_dealloc,
+	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+};
+
+// perf_cpu_map declarations
+typedef struct {
+	PyObject_HEAD
+	struct perf_cpu_map *ptr;
+} py_perf_cpu_map;
+
+static void py_perf_cpu_map_dealloc(py_perf_cpu_map *cpu_map)
+{
+	free(cpu_map->ptr);
+	Py_DECREF(cpu_map);
+	PyObject_Del((PyObject *)cpu_map);
+}
+
+static PyTypeObject py_perf_cpu_map_type = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+	.tp_name = "libperf.py_perf_cpu_map",
+	.tp_doc = "Perf cpu_map object",
+	.tp_basicsize = sizeof(py_perf_cpu_map),
+	.tp_dealloc = (destructor)py_perf_cpu_map_dealloc,
+	.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+};
+
 static void
 python_push_type(const char *name, PyObject *module, PyTypeObject *type)
 {
--- tools/lib/perf/py_perf.c
+++ tools/lib/perf/py_perf.c
@@ -2,18 +2,20 @@
 #include "include/perf/py_perf.h"
 #include <stdlib.h>
 #include <perf/threadmap.h>
+#include <perf/evlist.h>
+#include <perf/evsel.h>
 #include <perf/py_perf.h>
 #include <errno.h>
 #include <internal/threadmap.h>
 
 static PyObject *program_perf_thread_map__new_dummy(PyObject *self, PyObject *args)
 {
-	//py_perf_thread_map *pythread_map = PyObject_New(py_perf_thread_map, &py_perf_thread_map_type);
-	py_perf_thread_map *pythread_map = NULL;
+	py_perf_thread_map *pythread_map = PyObject_New(py_perf_thread_map, &py_perf_thread_map_type);
+	//py_perf_thread_map *pythread_map = NULL;
 
-	if (!PyArg_ParseTuple(args, "O", &pythread_map)) {
+	/*if (!PyArg_ParseTuple(args, "O", &pythread_map)) {
 		return NULL;
-	}
+	}*/
 
 	pythread_map->ptr = perf_thread_map__new_dummy();
 	if (!pythread_map->ptr) {
@@ -22,6 +24,92 @@ static PyObject *program_perf_thread_map__new_dummy(PyObject *self, PyObject *ar
 	}
 
 	return Py_BuildValue("O", pythread_map);
+}
+
+static PyObject *program_perf_thread_map__set_pid(PyObject *self, PyObject *args)
+{
+	py_perf_thread_map *pythread_map = NULL;
+	int idx, pid;
+
+	if (!PyArg_ParseTuple(args, "Oii", &pythread_map, &idx, &pid)) {
+		return NULL;
+	}
+
+	perf_thread_map__set_pid(pythread_map->ptr, idx, pid);
+
+	return Py_None;
+}
+
+static PyObject *program_perf_evlist__new(PyObject *self, PyObject *args)
+{
+	py_perf_evlist *pyperf_evlist = PyObject_New(py_perf_evlist, &py_perf_evlist_type);
+
+	pyperf_evlist->ptr = perf_evlist__new();
+	if (!pyperf_evlist->ptr) {
+		Py_DECREF(pyperf_evlist);
+		return Py_None;
+	}
+
+	return Py_BuildValue("O", pyperf_evlist);
+}
+
+static PyObject *program_perf_evsel__new(PyObject *self, PyObject *args)
+{
+	struct perf_event_attr *attr;
+	py_perf_evsel *pyperf_evsel = PyObject_New(py_perf_evsel, &py_perf_evsel_type);
+
+	if (!PyArg_ParseTuple(args, "O", &attr)) {
+		return NULL;
+	}
+
+	pyperf_evsel->ptr = perf_evsel__new(attr);
+	if (!pyperf_evsel->ptr) {
+		Py_DECREF(pyperf_evsel);
+		return Py_None;
+	}
+
+	return Py_BuildValue("O", pyperf_evsel);
+}
+
+static PyObject *program_perf_evlist__add(PyObject *self, PyObject *args)
+{
+	struct perf_evsel *evsel;
+	struct perf_evlist *evlist;
+	PyObject *pyevlist, *pyevsel;
+
+	if (!PyArg_ParseTuple(args, "OO", &pyevlist, &pyevsel)) {
+		return NULL;
+	}
+
+	//Py_INCREF(pyevlist);
+	//Py_INCREF(pyevsel);
+	evsel = (pyevsel == Py_None)? NULL: ((py_perf_evsel *)pyevsel)->ptr;
+	evlist = (pyevlist == Py_None)? NULL: ((py_perf_evlist *)pyevlist)->ptr;
+	perf_evlist__add(evlist, evsel);
+
+	return Py_None;
+}
+
+static PyObject *program_perf_evlist__set_maps(PyObject *self, PyObject *args)
+{
+	struct perf_thread_map *thread_map;
+	struct perf_evlist *evlist;
+	struct perf_cpu_map *cpu_map;
+	PyObject *pyevlist, *pythread_map, *pycpu_map;
+
+	if (!PyArg_ParseTuple(args, "OOO", &pyevlist, &pycpu_map, &pythread_map)) {
+		return NULL;
+	}
+
+	//Py_INCREF(pyevlist);
+	//Py_INCREF(pythread_map);
+	//Py_INCREF(pycpu_map);
+	evlist = (pyevlist == Py_None)? NULL: ((py_perf_evlist *)pyevlist)->ptr;
+	cpu_map = (pycpu_map == Py_None)? NULL: ((py_perf_cpu_map *)pycpu_map)->ptr;
+	thread_map = (pythread_map == Py_None)? NULL: ((py_perf_thread_map *)pythread_map)->ptr;
+	perf_evlist__set_maps(evlist, cpu_map, thread_map);
+
+	return Py_None;
 }
 
 static int libperf_print(enum libperf_print_level level,
@@ -38,6 +126,11 @@ static PyObject *program_libperf_init(PyObject *self, PyObject *args)
 
 PyMethodDef libperf_methods[] = {
 	{"perf_thread_map__new_dummy", program_perf_thread_map__new_dummy, METH_VARARGS, "Create a dummy thread map function variable"},
+	{"perf_thread_map__set_pid", program_perf_thread_map__set_pid, METH_VARARGS, "Set PID for a thread map"},
+	{"perf_evlist__new", program_perf_evlist__new, METH_VARARGS, "Create a perf evlist"},
+	{"perf_evsel__new", program_perf_evsel__new, METH_VARARGS, "Create a perf evsel"},
+	{"perf_evlist__add", program_perf_evlist__add, METH_VARARGS, "Add evsel to evlist"},
+	{"perf_evlist__set_maps", program_perf_evlist__set_maps, METH_VARARGS, "perf_evlist__set_maps"},
 	{"libperf_init", program_libperf_init, METH_VARARGS, "libperf init"},
 	{NULL, NULL, 0, NULL}
 };
@@ -57,6 +150,9 @@ PyMODINIT_FUNC PyInit_libperf(void) {
 		return NULL;
 
 	python_push_type("py_perf_thread_map", m, &py_perf_thread_map_type);
+	python_push_type("py_perf_evlist", m, &py_perf_evlist_type);
+	python_push_type("py_perf_evsel", m, &py_perf_evsel_type);
+	python_push_type("py_perf_cpu_map", m, &py_perf_cpu_map_type);
 
 	return m;
 }
